64987fef991d55406eba1080f7d5cc624f45217f
shivateja18 <shivateja0719@gmail.com>
2022-09-25 15:28:50 +0530

    Pre final code

--- Details/model.go
+++ Details/model.go
@@ -1,14 +1,14 @@
 package Details
 
-import "gorm.io/gorm"
+import . "gorm.io/gorm"
 
 type EMP struct {
-	gorm.Model
+	Model
 	Name     string `json:"name"`
 	Password string `json:"password"`
 	City     string `json:"city"`
 }
 
 type Handler struct {
-	DB *gorm.DB
+	DB *DB
 }
--- Route/route.go
+++ Route/route.go
@@ -1,27 +1,10 @@
 package Route
 
 import (
-	"EMP_API/Details"
+	. "EMP_API/Details"
+	. "EMP_API/Methods"
 	"fmt"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"log"
 )
-
-func Connect() {
-	const DNS = "host = 'localhost' port = '5432' dbname = 'API' user = 'postgres' password = 'Shiva@205101' sslmode = 'prefer'"
-	Database, err := gorm.Open(postgres.Open(DNS), &gorm.Config{})
-
-	if err != nil {
-		fmt.Println(err)
-	}
-	if err == nil {
-		log.Print("Connected")
-	}
-	_ = Database.AutoMigrate(&Details.EMP{})
-
-	DB = Database
-}
 
 func Initialize() {
 
