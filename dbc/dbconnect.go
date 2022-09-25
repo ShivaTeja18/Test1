5d89f254a34d313938cbea9d4d9a6df1d1d5748f
shivateja18 <shivateja0719@gmail.com>
2022-09-25 15:27:40 +0530

    Pre final code

---
+++ dbc/dbconnect.go
@@ -0,0 +1,24 @@
+package dbc
+
+import (
+	. "EMP_API/Details"
+	. "EMP_API/Methods"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
+
+func Connect() {
+	const DNS = "host = 'localhost' port = '5432' dbname = 'API' user = 'postgres' password = 'Shiva@205101' sslmode = 'prefer'"
+	Database, err := gorm.Open(postgres.Open(DNS), &gorm.Config{})
+
+	if err != nil {
+		log.Println(err)
+	}
+	if err == nil {
+		log.Print("Connected")
+	}
+	_ = Database.AutoMigrate(&EMP{})
+
+	DB = Database
+}
